ce42a24c9dd63c958bba6e776d2df0fc09da0761
ronaldoapf <ronaldo.alves.1997@gmail.com>
2023-05-08 09:19:17 -0300

    feat: create landingPage and Register route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,15 @@
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import { LandingPage } from '@pages/LandingPage'
 import { Login } from '@pages/Login'
+import { Register } from '@pages/Register'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
